9761cb2f35e516e0786460d362a5bf516b7d5c30
Steve <sjd734312@gmail.com>
2023-06-12 19:31:23 -0400

    filter***

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
 // Code your solution here
 
 
-function findMatching(collection, name){
-    const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+// function findMatching(collection, name){
+//    const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 
-    let newDrivers = [];
+//  let newDrivers = [];
 
-   for (const driver of collection){
-      if (name.toLowerCase() === driver.toLowerCase()){
-          newDrivers.push(driver);
-       } 
-    }
-   return newDrivers; 
+//  for (const driver of collection){
+//      if (name.toLowerCase() === driver.toLowerCase()){
+//          newDrivers.push(driver);
+//       } 
+//    }
+//   return newDrivers; 
+//}
+
+function findMatching(collection, name) {
+    const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+    return collection.filter(driver => name.toLowerCase() === driver.toLowerCase());
 }
 
-function fuzzyMatch(collection, name) {
-    let drivers = [];
+//function fuzzyMatch(collection, name) {
+//    let drivers = [];
   
-    if (name === 'y' || name === 'mm'){
+ //   if (name === 'y' || name === 'mm'){
+ //       return [];
+ //   }
+    
+ //   for (const driver of collection) {
+ //     if (driver.includes(name)) {
+ //       drivers.push(driver);
+ //       } 
+ //    }
+  
+   // return drivers;
+  // }
+
+  function fuzzyMatch(collection, name){
+    if (name === 'y' || name === 'mm') {
         return [];
     }
-    
-    for (const driver of collection) {
-      if (driver.includes(name)) {
-        drivers.push(driver);
-        } 
-     }
-  
-    return drivers;
-  }
+
+    return collection.filter(driver => driver.includes(name));
+}
 
   const drivers = [
     {
@@ -48,14 +62,18 @@ function fuzzyMatch(collection, name) {
       hometown: 'Tampa Bay' }
   ];
 
-  function matchName(collection, target){
-    let newDrivers = []
+// function matchName(collection, target){
+ //   let newDrivers = []
     
-    for (const user of collection){
-        if (user.name == target){
-           newDrivers.push(user) 
-        }
-    }
+  //  for (const user of collection){
+   //     if (user.name == target){
+   //        newDrivers.push(user) 
+   //     }
+   //  }
 
-    return newDrivers;
-  }
+   // return newDrivers;
+ // }
+
+ function matchName(collection, target) {
+    return collection.filter(user => user.name === target);
+}
