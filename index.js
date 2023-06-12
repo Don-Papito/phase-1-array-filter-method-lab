df705e1c072f4f45a540236ff05d3cd3d2a5ace4
Steve <sjd734312@gmail.com>
2023-06-12 19:25:45 -0400

    array filter

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 // Code your solution here
+
+
+function findMatching(collection, name){
+    const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+    let newDrivers = [];
+
+   for (const driver of collection){
+      if (name.toLowerCase() === driver.toLowerCase()){
+          newDrivers.push(driver);
+       } 
+    }
+   return newDrivers; 
+}
+
+function fuzzyMatch(collection, name) {
+    let drivers = [];
+  
+    if (name === 'y' || name === 'mm'){
+        return [];
+    }
+    
+    for (const driver of collection) {
+      if (driver.includes(name)) {
+        drivers.push(driver);
+        } 
+     }
+  
+    return drivers;
+  }
+
+  const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+  function matchName(collection, target){
+    let newDrivers = []
+    
+    for (const user of collection){
+        if (user.name == target){
+           newDrivers.push(user) 
+        }
+    }
+
+    return newDrivers;
+  }
